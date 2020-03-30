3d71e4b035b3a280450e2c6065bf48c03b70d77f
Sagnik Mazumder <39977582+Karnak123@users.noreply.github.com>
2020-03-30 13:50:38 +0530

    Update rgb_align_ssd.m

--- rgb_align_ssd.m
+++ rgb_align_ssd.m
@@ -6,11 +6,6 @@ B = img(1:rr,:);
 G = img((rr+1):(2*rr),:);
 R = img((2*rr+1):r,:);
 
-ref_img_region = G;
-[rg,cg] = size(ref_img_region);
-ref_img_region = ref_img_region(ceil((rg-50)/2) :ceil((rg-50)/2) + 50,ceil((cg-50)/2) :ceil((cg-50)/2) + 50);
-ref_img_region = double(ref_img_region);
-
 r = align(G,R);
 b = align(G,B);
 ColorImg_aligned = cat(3,r,G,b);
